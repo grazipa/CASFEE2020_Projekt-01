c4568786a205c5b34a4005af104706eed2629da3
Grazioli, Patrick <grazioli@netcloud.ch>
2020-05-30 21:54:13 +0200

    Added style to modal and temporary onclick action to change style.

--- project/scripts/scripts.js
+++ project/scripts/scripts.js
@@ -1,25 +1,47 @@
- // Get the modal
- var modal = document.getElementById("myModal");
-            
- // Get the button that opens the modal
- var btn = document.getElementById("myBtn");
- 
- // Get the <span> element that closes the modal
- var span = document.getElementsByClassName("close")[0];
- 
- // When the user clicks the button, open the modal 
- btn.onclick = function() {
-   modal.style.display = "block";
- }
- 
- // When the user clicks on <span> (x), close the modal
- span.onclick = function() {
-   modal.style.display = "none";
- }
- 
- // When the user clicks anywhere outside of the modal, close it
- window.onclick = function(event) {
-   if (event.target == modal) {
-     modal.style.display = "none";
-   }
- }
+//Check for session
+if ('style' in localStorage) {
+
+  // Check if correct style
+  if (localStorage.getItem('style') !== document.getElementById('websiteStyle').options[document.getElementById('websiteStyle').selectedIndex].value) {
+    switch (localStorage.getItem('style')) {
+      case 'grey':
+        document.getElementById('websiteStyle').selectedIndex = 0;
+        document.getElementById('stylesheet').href = './css/grey_styles.css';
+        localStorage.setItem('style', 'grey');
+        break;
+      
+      case 'blue':
+        document.getElementById('websiteStyle').selectedIndex = 1;
+        document.getElementById('stylesheet').href = './css/blue_styles.css';
+        localStorage.setItem('style', 'blue');
+        break;
+    }
+  }
+
+} else {
+  localStorage.setItem('style', document.getElementById('websiteStyle').options[document.getElementById('websiteStyle').selectedIndex].value);
+}
+
+// Change mode
+document.getElementById('websiteStyle').onchange = function () {
+  switch (document.getElementById('websiteStyle').options[document.getElementById('websiteStyle').selectedIndex].value) {
+    case 'grey':
+      document.getElementById('stylesheet').href = './css/grey_styles.css';
+      localStorage.setItem('style', 'grey');
+      break;
+    
+    case 'blue':
+      document.getElementById('stylesheet').href = './css/blue_styles.css';
+      localStorage.setItem('style', 'blue');
+      break;
+  }
+};
+
+// Open modal
+document.getElementById('newNote').onclick = function() {
+  document.getElementById('newNoteModal').style.display = 'grid';
+}
+
+document.getElementById('cancel').onclick = function() {
+  document.getElementById('newNoteModal').style.display = 'none';
+}
